fdab45f5873b9e4a9dd76e4ca45d9d2cfb1ee662
elisabethfarfan <elisabethfarfan@gmail.com>
2022-09-07 22:33:51 -0500

    agregando estilos al form de login

--- burgerqueen/src/app/app-routing.module.ts
+++ burgerqueen/src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
-import { NgModule } from '@angular/core';
+ //import de modulos del router de angular
+ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
+//import de componentes 
 import { PedidosComponent } from './components/pedidos/pedidos.component';
 import { ControlPedidosComponent } from './components/control-pedidos/control-pedidos.component';
 import { HistorialPedidosComponent } from './components/historial-pedidos/historial-pedidos.component';
 import { LoginComponent } from './components/login/login.component';
 import { ProductosComponent } from './components/productos/productos.component';
 
-
+//array de rutas
 const routes: Routes = [
   {path:'', component: LoginComponent},
   {path:'pedidos', component: PedidosComponent},
@@ -16,6 +19,7 @@ const routes: Routes = [
   {path:'**', redirectTo:'pedidos', pathMatch:'full'}
 ];
 
+//exportar el modulo
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
